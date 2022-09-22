6d87da646414814138027d87044e0ee188c0228e
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-22 09:12:05 +0200

    n6

--- snack6.php
+++ snack6.php
@@ -22,10 +22,7 @@
       ]
     ]
   ];
-  
-  var_dump($db);
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,18 +30,42 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>teachers$pm</title>
+  <link rel="stylesheet" href="./snack6.css">
+  <title>Snack6</title>
 </head>
 <body>
-  <div>
-    <?php
-    
-
-
-    ?>
+  <div class="gray">
+    <ul>
+      <?php 
+        for($i = 0; $i < count($db['teachers']); $i++) {
+          $teacher = $db['teachers'][$i];
+          ?>
+          <li>
+            <?php echo "{$teacher['name']} {$teacher['lastname']}"?>
+          </li> 
+          <?php
+        }
+      ?>
+    </ul>
   </div>
+  <div class="green">
+    <ul>
+      <?php 
+        for($i = 0; $i < count($db['pm']); $i++) {
+          $student = $db['pm'][$i];
+          ?>
+          <li>
+            <?php echo "{$student['name']} {$student['lastname']}"?>
+          </li> 
+          <?php
+        }
+      ?>
+    </ul>
+  </div>
+  
 </body>
 </html>
+
 
 
 
